6408d48a4027e15b8b1c439ae33ca2458a5b0a5b
Alexandr Bokov <pd.sk@bk.ru>
2026-04-02 09:24:21 +0300

    Create main page + post page

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -3,11 +3,9 @@ import { Post } from "./components/Post";
 import * as SC from "./styles";
 
 export const Posts = ({ posts }) => (
-  <>
     <SC.Posts>
       {posts.map((post) => (
         <Post key={post.id} post={post} />
       ))}
     </SC.Posts>
-  </>
 );
