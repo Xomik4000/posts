029e207e8bed1a89976a8f19ebd16eabbf920f17
Alexandr Bokov <pd.sk@bk.ru>
2026-04-02 10:40:03 +0300

    Create detail post page + new UI component - Link

--- src/pages/posts/details/index.jsx
+++ src/pages/posts/details/index.jsx
@@ -1,3 +1,32 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { useParams } from "react-router-dom";
+import { INITIAL_POST } from "..";
+import { Typo } from "../../../components/Typo";
+import { Container } from "../../../components/Container";
+import { Link } from "../../../components/Link";
+import * as SC from "./styles";
 
-export const DetailPostPage = () => <>это детальная страничка постов</>
+export const DetailPostPage = () => {
+  const { id } = useParams();
+
+  const currentPost = useMemo(
+    () => INITIAL_POST.find((item) => item.id === Number(id)),
+    [id],
+  );
+
+  if (!currentPost) {
+    return <>Пост не найден</>;
+  }
+
+  return (
+    <Container>
+      <Typo>{currentPost.title}</Typo>
+      <SC.Image src={currentPost.image} alt={currentPost.title} />
+      <SC.Text>{currentPost.text}</SC.Text>
+      <div style={{ clear: "both" }}></div>
+      <SC.LinkWrapper>
+        <Link to="/posts/">Обратно к публикациям</Link>
+      </SC.LinkWrapper>
+    </Container>
+  );
+};
--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -1,44 +1,50 @@
 import React from "react";
 import { Posts } from "../../components/Posts";
 import { Container } from "../../components/Container";
-import { Typo } from "../../components/Typo"
+import { Typo } from "../../components/Typo";
 
-const INITIAL_POST = [
+export const INITIAL_POST = [
   {
     id: 1,
     title: "Post 1",
     image:
       "https://foxtime.ru/wp-content/uploads/fly-images/83688/65-1-2190x1230.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore dicta hic nihil saepe sed in cupiditate laborum nulla itaque repellat odio expedita culpa, deserunt ex necessitatibus placeat facilis. Exercitationem, repellendus.",
   },
   {
     id: 2,
     title: "Post 2",
     image:
       "https://foxtime.ru/wp-content/uploads/fly-images/83688/65-1-2190x1230.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore dicta hic nihil saepe sed in cupiditate laborum nulla itaque repellat odio expedita culpa, deserunt ex necessitatibus placeat facilis. Exercitationem, repellendus.",
   },
   {
     id: 3,
     title: "Post 3",
     image:
       "https://foxtime.ru/wp-content/uploads/fly-images/83688/65-1-2190x1230.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore dicta hic nihil saepe sed in cupiditate laborum nulla itaque repellat odio expedita culpa, deserunt ex necessitatibus placeat facilis. Exercitationem, repellendus.",
   },
   {
     id: 4,
     title: "Post 4",
     image:
       "https://foxtime.ru/wp-content/uploads/fly-images/83688/65-1-2190x1230.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore dicta hic nihil saepe sed in cupiditate laborum nulla itaque repellat odio expedita culpa, deserunt ex necessitatibus placeat facilis. Exercitationem, repellendus.",
   },
   {
     id: 5,
     title: "Post 5",
     image:
       "https://foxtime.ru/wp-content/uploads/fly-images/83688/65-1-2190x1230.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore dicta hic nihil saepe sed in cupiditate laborum nulla itaque repellat odio expedita culpa, deserunt ex necessitatibus placeat facilis. Exercitationem, repellendus.",
   },
   {
     id: 6,
     title: "Post 6",
     image:
       "https://foxtime.ru/wp-content/uploads/fly-images/83688/65-1-2190x1230.jpg",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore dicta hic nihil saepe sed in cupiditate laborum nulla itaque repellat odio expedita culpa, deserunt ex necessitatibus placeat facilis. Exercitationem, repellendus.",
   },
 ];
 
